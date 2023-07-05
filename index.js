e4e9e919a7a3dfd4d0b78eea062dfe8e9af570fe
Maaaaaaaaht <125898593+Maaaaaaaaht@users.noreply.github.com>
2023-07-05 01:34:30 +0100

    add css file and start styling

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
+const getRandomNumber = () => Math.floor(Math.random() * 10);
+
 const getNumber = () => {
-    // get random lucky number
-    document.getElementById("randomNumber").innerHTML = 
-    Math.floor(Math.random() * 10);
-    // get the first ball's number
-    document.getElementById("ball1").innerHTML = 
-    Math.floor(Math.random() * 10);
-    // get the second ball's number 
-    document.getElementById("ball2").innerHTML = 
-    Math.floor(Math.random() * 10);
-    // get the third ball's number 
-    document.getElementById("ball3").innerHTML = 
-    Math.floor(Math.random() * 10);
-    // get the fourth ball's number
-    document.getElementById("ball4").innerHTML = 
-    Math.floor(Math.random() * 10);
-    // get the fifth ball's number 
-    document.getElementById("ball5").innerHTML = 
-    Math.floor(Math.random() * 10);
-}
+  // get random lucky number
+  document.getElementById("randomNumber").innerHTML = getRandomNumber();
+  // get the first ball's number
+  document.getElementById("ball1").innerHTML = getRandomNumber();
+  // get the second ball's number
+  document.getElementById("ball2").innerHTML = getRandomNumber();
+  // get the third ball's number
+  document.getElementById("ball3").innerHTML = getRandomNumber();
+  // get the fourth ball's number
+  document.getElementById("ball4").innerHTML = getRandomNumber();
+  // get the fifth ball's number
+  document.getElementById("ball5").innerHTML = getRandomNumber();
+};
+
